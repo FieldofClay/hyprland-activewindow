9ba7a2f429c323a1b770ac476066a118ef1de1ea
FieldofClay <7278759+FieldofClay@users.noreply.github.com>
2024-02-18 13:56:01 +1100

    Added logging

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use serde::Serialize;
 use serde_json::json;
 use std::env;
 use std::sync::Arc;
+use flexi_logger::{Logger, FileSpec};
+use log;
 
 const HELP: &str = "\
 hyprland-activewindow: a multi monitor aware active hyprland window title reporter, designed to be used with eww.
@@ -48,12 +50,18 @@ impl WindowPrinter {
     fn print_single(&self) -> Result<()> {
         let active_workspace_id = Monitors::get()?
             .find(|m| m.name == self.mon.to_string())
-            .ok_or_else(|| HyprError::NotOkDispatch("No monitor found".to_string()))?
+            .ok_or_else(|| {
+                log::error!("No monitor found with name: {}", self.mon);
+                HyprError::NotOkDispatch("No monitor found".to_string())
+            })?
             .active_workspace
             .id;
         let title = Workspaces::get()?
             .find(|w| w.id == active_workspace_id)
-            .ok_or_else(|| HyprError::NotOkDispatch("No workspace found".to_string()))?
+            .ok_or_else(|| {
+                log::warn!("No workspace found with ID: {}", active_workspace_id);
+                HyprError::NotOkDispatch("No workspace found".to_string())
+            })?
             .last_window_title;
         println!("{}", title);
         Ok(())
@@ -65,11 +73,17 @@ impl WindowPrinter {
         for monitor in monitors {
             let workspace = Workspaces::get()?
                 .find(|w| w.id == monitor.active_workspace.id)
-                .ok_or_else(|| HyprError::NotOkDispatch("No active workspace found".to_string()))?;
+                .ok_or_else(|| {
+                    log::error!("No active workspace found. ID: {}", monitor.active_workspace.id);
+                    HyprError::NotOkDispatch("No active workspace found".to_string())
+                })?;
 
             let client = Clients::get()?
                 .find(|c| c.address == workspace.last_window)
-                .ok_or_else(|| HyprError::NotOkDispatch("Unable to get last window".to_string()))?;
+                .ok_or_else(|| {
+                    log::warn!("Unable to get last window");    
+                    HyprError::NotOkDispatch("Unable to get last window".to_string())}
+                )?;
 
             let mc: MonitorCustom = MonitorCustom {
                 name: monitor.name,
@@ -83,21 +97,44 @@ impl WindowPrinter {
     }
 }
 
-fn main() -> Result<()> {
+fn main() -> Result<()>{
     let args: Vec<String> = env::args().collect();
     //check args
     if args.len() != 2 || args[1].eq("-h") || args[1].eq("--help") {
         println!("{HELP}");
         std::process::exit(0);
     }
+    
+    let _logger = match Logger::try_with_str("info") {
+        Ok(logger) => {
+            match logger.log_to_file(FileSpec::default()
+            .directory("/tmp")
+            .basename("hyprland-activewindow"))
+            .start() {
+                Ok(logger) => {
+                    logger
+                },
+                Err(e) => {
+                    println!("Unable to start logger: {}", e);
+                    std::process::exit(1)
+                }
+            };
+        },
+        Err(e) => {
+            println!("Unable to initialise logger: {}", e);
+            std::process::exit(1)
+        }
+    };
+        
     let mon = args[1].to_string();
     let mon_object = Monitors::get()?
         .find(|m| m.name == mon);
     if mon_object.is_none() && mon != "_" {
-        println!("Unable to find monitor {mon}");
+        log::error!("Unable to find monitor {mon}");
         std::process::exit(0);
     }
 
+    log::info!("Started with arg {}", mon);
     let wp = Arc::new(WindowPrinter::new(mon));
     wp.print();
 
